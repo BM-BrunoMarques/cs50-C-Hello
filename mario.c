6183a896398eb6925b61482c78031620e55a83c0
BM-BrunoMarques <bruno.fdm93@gmail.com>
2020-09-27 07:05:19 +0000

    increased space between pyramids

--- mario.c
+++ mario.c
@@ -7,10 +7,11 @@ int main(void)
 {
     int n;
     //this will prompt the user for a number between 1-8
-    do{
-        n = get_int("Please insert a number for height between 1 - 8:\n");
+    do
+    {
+        n = get_int("Please insert a number for height between 1 - 8: \n");
     }
-    while( n < 1 || n > 8);
+    while (n < 1 || n > 8);
 
 
     //Vars declared here for 'for loop'.
@@ -18,11 +19,12 @@ int main(void)
     char cardinal = '#';
 
     //Will run n times, n is the user input for height
-    for( int i = 1 ; i <= n ; i++ ){
+    for (int i = 1 ; i <= n ; i++)
+    {
 
-        printThis(space, n-i);
+        printThis(space, n - i);
         printThis(cardinal, i);
-        printThis(space, 1);     // space between pyramid sides
+        printThis(space, 2);     // space between pyramid sides
         printThis(cardinal, i);
 
         printf("\n");
@@ -31,8 +33,10 @@ int main(void)
 
 
 //this function will print a character as 'ch' n times.
-int printThis(char ch, int n){
-    for( int j = 0; j < n; j++){
+int printThis(char ch, int n)
+{
+    for (int j = 0; j < n; j++)
+    {
         printf("%c", ch);
     }
     return 0;
