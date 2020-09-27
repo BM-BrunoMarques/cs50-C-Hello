66b135bf84702c9770b3ea3394e84d3f64f2ca9a
BM-BrunoMarques <bruno.fdm93@gmail.com>
2020-09-27 05:52:17 +0000

    hello v2

--- hello.c
+++ hello.c
@@ -4,6 +4,8 @@
 
 int main(void)
 {
+    //this will hold the user name
     string name;
+    //print greeting user name. "get_string" is from cs50
     printf("hello, %s\n", name = get_string("What's your name?\n"));
 }
